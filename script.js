8fb00cbed092fe9a8ee9674f6f4267eabc3e885d
Akash-tech9163 <68282065+Akash-tech9163@users.noreply.github.com>
2021-07-21 11:15:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var a=prompt("Enter UserName");
- var b=prompt("Enter password");
-  	if(a== "shubhangi" && b=="22072000")
-  	{
-  	    window.location.href="start.html";
-  	}
-  	else
-  	{
-  		alert("invalid details");
-  		window.location.href="index.html";
-  	}
+// var a=prompt("Enter UserName");
+//  var b=prompt("Enter password");
+//   	if(a== "shubhangi" && b=="22072000")
+//   	{
+//   	    window.location.href="start.html";
+//   	}
+//   	else
+//   	{
+//   		alert("invalid details");
+//   		window.location.href="index.html";
+//   	}
   	
